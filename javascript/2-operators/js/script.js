3880fa6404d89932747a10404c0fb1127a96964f
Mateus <mateusc.ramos@outlook.com>
2024-01-07 17:31:43 -0300

    js-types: special numbers

--- javascript/2-operators/js/script.js
+++ javascript/2-operators/js/script.js
@@ -9,3 +9,12 @@ console.log(10 - 3);
 console.log(10 * 3);
 console.log(10 / 3);
 console.log(5 + 10 * 2);
+
+// Special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+console.log(10 / 0);
+console.log(10 / -0);
+console.log(10 / "hello");
