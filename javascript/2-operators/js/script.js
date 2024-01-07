b8cd8b48f9180639c2d38d03a4aeb2e22c272fc0
Mateus <mateusc.ramos@outlook.com>
2024-01-07 17:28:14 -0300

    js-types: arithmetic operators

--- javascript/2-operators/js/script.js
+++ javascript/2-operators/js/script.js
@@ -2,3 +2,10 @@
 console.log(typeof 10);
 console.log(typeof 3.14);
 console.log(typeof -2);
+
+// Arithmetic operators
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(5 + 10 * 2);
