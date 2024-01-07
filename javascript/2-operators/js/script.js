a07055a1e0d893262a9397dd4c8ff04cc90e0b80
Mateus <mateusc.ramos@outlook.com>
2024-01-07 17:53:37 -0300

    js-types: special characters

--- javascript/2-operators/js/script.js
+++ javascript/2-operators/js/script.js
@@ -22,3 +22,8 @@ console.log(10 / "hello");
 // Strings
 console.log(typeof "hello");
 console.log("Hello, world");
+
+// Special Characters
+console.log("Hello, \"world\"");
+console.log("Hello, \nworld");
+console.log("Hello, \tworld");
