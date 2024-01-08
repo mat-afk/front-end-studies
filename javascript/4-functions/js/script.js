c660f5b33f5723c93c755383307420685b856db7
Mateus <mateusc.ramos@outlook.com>
2024-01-08 20:14:35 -0300

    js-functions: scope

--- javascript/4-functions/js/script.js
+++ javascript/4-functions/js/script.js
@@ -19,8 +19,20 @@ say("Hello =]");
 say("Bye =]");
 say("See you later =]");
 
+// Returning values
 function sum(n1, n2) {
   return n1 + n2;
 }
 
-console.log(sum(1, 2));
+console.log("The sum is", sum(1, 2));
+
+// Function scope
+let y = 10;
+
+function newY() {
+  let y = 20;
+  console.log("Inside this function, the value of y is", y);
+}
+
+newY();
+console.log("Outside the function, the value of y is", y);
