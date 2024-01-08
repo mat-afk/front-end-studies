812fafc6c916f0f8b9808162da21910595efafc1
Mateus <mateusc.ramos@outlook.com>
2024-01-08 19:15:14 -0300

    js-functions: creating functions

---
+++ javascript/4-functions/js/script.js
@@ -0,0 +1,20 @@
+// Creating a function
+function sayHello() {
+  console.log("Hello =]");
+}
+
+sayHello();
+
+const sayBye = function () {
+  console.log("Bye =]");
+};
+
+sayBye();
+
+function say(message) {
+  console.log(message);
+}
+
+say("Hello =]");
+say("Bye =]");
+say("See you later =]");
