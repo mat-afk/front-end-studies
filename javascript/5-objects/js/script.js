6a47d6afd72d9a1817d71d86452b44c549caf158
Mateus <mateusc.ramos@outlook.com>
2024-01-09 04:20:13 -0300

    js-objects: properties

--- javascript/5-objects/js/script.js
+++ javascript/5-objects/js/script.js
@@ -1,3 +1,12 @@
+// Arrays
 const array = [1, 2, 3, 4, 5];
-
 console.log(array, typeof array);
+
+// Properties
+const users = ["John", "Mary", "Peter"];
+console.log(users.length);
+console.log(users["length"]);
+
+console.log(users[0]);
+console.log(users[1]);
+console.log(users[2]);
