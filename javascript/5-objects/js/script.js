802218a5ce7caa127203f871ce360359d8e0a91c
Mateus <mateusc.ramos@outlook.com>
2024-01-09 18:05:00 -0300

    js-objects: rest operator

--- javascript/5-objects/js/script.js
+++ javascript/5-objects/js/script.js
@@ -191,3 +191,12 @@ console.log("Letters joined:", letters.join(""));
 
 // Repeat
 console.log("hello ".repeat(5));
+
+// Rest operator
+const sum = (...nums) => {
+  let sum = 0;
+  nums.forEach((num) => (sum += num));
+  return sum;
+};
+
+console.log("The sum is:", sum(1, 2, 3, 7, 20, 19, 46, 780));
