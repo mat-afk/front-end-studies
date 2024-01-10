b512db83838fe98d4ee80c97929ed19ce68a678b
Mateus <mateusc.ramos@outlook.com>
2024-01-10 19:53:36 -0300

    js-debug: log

--- javascript/7-debug/js/script.js
+++ javascript/7-debug/js/script.js
@@ -7,3 +7,12 @@ const greet = "Hello World!";
 // const undefined = 0;
 
 // delete [].length;
+
+// Log
+let a = 1;
+let b = 2;
+
+for (let i = 0; i < b; i++) {
+  a += i + 2;
+  console.log(a);
+}
