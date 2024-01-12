5bedf3ba95b167b7048c2a98db8a80263271624d
mat-afk <mateusc.ramos@outlook.com>
2024-01-12 15:35:53 -0300

    js-dom: selecting elements

--- javascript/9-DOM/js/script.js
+++ javascript/9-DOM/js/script.js
@@ -8,3 +8,24 @@ console.log(
   "Text content of H1:",
   document.body.childNodes[1].childNodes[1].textContent
 );
+
+// Selecting elements
+
+// 1. By Tag name
+const listItems = document.getElementsByTagName("li");
+console.log("Elements selected by Tag name:", listItems);
+
+// 2. By Id
+const title = document.getElementById("title");
+console.log("Element selected by Id:", title);
+
+// 3. By Class
+const products = document.getElementsByClassName("product");
+console.log("Elements selected by Class:", products);
+
+// 4. By CSS selector
+const mainContainer = document.querySelector("#main-container");
+console.log("Element selected by CSS selector:", mainContainer);
+
+const productsQuery = document.querySelectorAll(".product");
+console.log("Elements selected by CSS selector:", productsQuery);
