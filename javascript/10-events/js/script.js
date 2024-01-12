53e13851609b2215e0b3be65c1311960f33c654a
mat-afk <mateusc.ramos@outlook.com>
2024-01-12 17:33:18 -0300

    js-events: removing event listeners

--- javascript/10-events/js/script.js
+++ javascript/10-events/js/script.js
@@ -5,3 +5,18 @@ btn.addEventListener("click", function () {
   count++;
   btn.textContent = count;
 });
+
+// Removing events
+const eventBtn = document.querySelector("#event-button");
+const removeBtn = document.querySelector("#remove-button");
+
+function eventHandler() {
+  console.log("You clicked the button!");
+}
+
+eventBtn.addEventListener("click", eventHandler);
+
+removeBtn.addEventListener("click", () => {
+  console.log("Removing event handler...");
+  eventBtn.removeEventListener("click", eventHandler);
+});
