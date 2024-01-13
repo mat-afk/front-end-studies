30474db55890edf9d899c6df6ff87080c3475bc2
Mateus <mateusc.ramos@outlook.com>
2024-01-13 16:12:10 -0300

    multitable-project: template and DOMParser

--- javascript/11-project-times-table/js/script.js
+++ javascript/11-project-times-table/js/script.js
@@ -3,10 +3,34 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const limitInput = document.querySelector("#limit");
 
+const title = document.querySelector("#multiplication-title span");
+
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
+
 // Functions
+const createTable = (number, limit) => {
+  multiplicationTable.innerHTML = "";
+
+  for (let i = 1; i <= limit; i++) {
+    const template = `<div class="row">
+        <div class="operation">${number} X ${i} =</div>
+        <div class="result">${number * i}</div>
+    </div>`;
+
+    const parser = new DOMParser();
+    const HTMLTemplate = parser.parseFromString(template, "text/html");
+
+    const row = HTMLTemplate.querySelector(".row");
+
+    multiplicationTable.appendChild(row);
+
+    title.innerHTML = number;
+  }
+};
 
 // Event listeners
-
 multiplicationForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -14,7 +38,6 @@ multiplicationForm.addEventListener("submit", (e) => {
   const limit = parseInt(limitInput.value);
 
   if (!number || !limit) return;
-  
 
-  console.log(number, limit);
+  createTable(number, limit);
 });
