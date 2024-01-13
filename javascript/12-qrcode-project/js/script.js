7609a3013f7047ce9dbf4ee4fde2e5f1b739c414
Mateus <mateusc.ramos@outlook.com>
2024-01-13 18:21:04 -0300

    qrcode-project: api and events

--- javascript/12-qrcode-project/js/script.js
+++ javascript/12-qrcode-project/js/script.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+const container = document.querySelector(".container");
+const btn = document.querySelector("#qr-form button");
+const textInput = document.querySelector("#qr-form input");
+const qrImage = document.querySelector("#qr-code img");
+
+const generateQRCode = () => {
+  const text = textInput.value;
+
+  if (!text) return;
+
+  btn.innerHTML = "Gerando código...";
+
+  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+
+  qrImage.addEventListener("load", () => {
+    container.classList.add("active");
+    btn.innerHTML = "Código gerado!";
+  });
+};
+
+btn.addEventListener("click", (e) => {
+  generateQRCode();
+});
+
+textInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") generateQRCode();
+});
+
+// Clean
+textInput.addEventListener("keyup", () => {
+  if (!textInput.value) {
+    container.classList.remove("active");
+    btn.innerHTML = "Gerar QR Code";
+  }
+});
