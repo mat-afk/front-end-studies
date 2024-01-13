217d4fa5550d30cf4aaa5d97b35c9c35754404b5
Mateus <mateusc.ramos@outlook.com>
2024-01-13 15:47:57 -0300

    multitable-project: submit event listener

--- javascript/11-project-times-table/js/script.js
+++ javascript/11-project-times-table/js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+// Selecting elements
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const limitInput = document.querySelector("#limit");
+
+// Functions
+
+// Event listeners
+
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const number = parseInt(numberInput.value);
+  const limit = parseInt(limitInput.value);
+
+  if (!number || !limit) return;
+  
+
+  console.log(number, limit);
+});
