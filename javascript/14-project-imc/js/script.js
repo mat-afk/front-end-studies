3cc3196d60f81b0fb00c9f7c6a0b31509d5a1c93
mat-afk <mateusc.ramos@outlook.com>
2024-01-15 18:26:53 -0300

    imc-project: calculate event

--- javascript/14-project-imc/js/script.js
+++ javascript/14-project-imc/js/script.js
@@ -5,6 +5,7 @@ const data = [
     range: "Menor que 18,5",
     classification: "Abaixo do peso",
     obesityLevel: "0",
+    class: "underweight",
   },
   {
     min: 18.5,
@@ -12,6 +13,7 @@ const data = [
     range: "Entre 18,5 e 24,9",
     classification: "Normal",
     obesityLevel: "0",
+    class: "normal",
   },
   {
     min: 25,
@@ -19,6 +21,7 @@ const data = [
     range: "Entre 25,0 e 29,9",
     classification: "Sobrepeso",
     obesityLevel: "I",
+    class: "overweight",
   },
   {
     min: 30,
@@ -26,6 +29,7 @@ const data = [
     range: "Entre 30,0 e 39,9",
     classification: "Obesidade",
     obesityLevel: "II",
+    class: "obesity",
   },
   {
     min: 40,
@@ -33,16 +37,23 @@ const data = [
     range: "Maior que 40,0",
     classification: "Obesidade grave",
     obesityLevel: "III",
+    class: "severe-obesity",
   },
 ];
 
-const imcTable = document.querySelector("#imc-table");
+const bmiTable = document.querySelector("#bmi-table");
 
 const heightInput = document.querySelector("#height");
 const weightInput = document.querySelector("#weight");
 
 const calculateBtn = document.querySelector("#calc-btn");
 const clearBtn = document.querySelector("#clear-btn");
+
+const bmiNumber = document.querySelector("#bmi-number span");
+const bmiInfo = document.querySelector("#bmi-info span");
+
+const calculateContainer = document.querySelector("#calc-container");
+const resultContainer = document.querySelector("#result-container");
 
 // Functions
 
@@ -64,8 +75,40 @@ function createTable(data) {
     div.appendChild(classification);
     div.appendChild(obesityLevel);
 
-    imcTable.appendChild(div);
+    bmiTable.appendChild(div);
   });
 }
 
-createTable(data);
+// Events
+
+calculateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  createTable(data);
+
+  const height = heightInput.value;
+  const weight = weightInput.value;
+  console.log(height, weight);
+
+  if (!height || !weight || height <= 0 || weight <= 0) {
+    return;
+  }
+
+  const bmi = weight / height ** 2;
+  console.log(bmi);
+
+  data.forEach((element) => {
+    if (bmi >= element.min && bmi <= element.max) {
+      bmiNumber.innerHTML = bmi.toFixed(2);
+      bmiNumber.classList.add(element.class || "normal");
+
+      bmiInfo.innerHTML = element.classification;
+      bmiInfo.classList.add(element.class || "normal");
+
+      console.log(bmiNumber, bmiInfo);
+    }
+  });
+
+  calculateContainer.classList.add("hide");
+  resultContainer.classList.remove("hide");
+});
