1ab884649ad5b78aa81b9f7dbf34f26c81ddd4bb
mat-afk <mateusc.ramos@outlook.com>
2024-01-16 13:56:12 -0300

    axios: http requests and responses

--- javascript/16-axios/js/script.js
+++ javascript/16-axios/js/script.js
@@ -1,2 +1,19 @@
 // Axios
 console.log(axios);
+
+// Requests
+const getUsers = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    console.log(response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getUsers();
