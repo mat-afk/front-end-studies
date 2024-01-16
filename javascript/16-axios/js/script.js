7b1c18bb9b300fe5e31105759e25f3cec90bb44b
mat-afk <mateusc.ramos@outlook.com>
2024-01-16 14:04:26 -0300

    axios: printing API data in DOM

--- javascript/16-axios/js/script.js
+++ javascript/16-axios/js/script.js
@@ -1,7 +1,7 @@
 // Axios
 console.log(axios);
 
-// Requests
+// Requests and responses
 const getUsers = async () => {
   try {
     const response = await axios.get(
@@ -16,4 +16,25 @@ const getUsers = async () => {
   }
 };
 
-getUsers();
+// API data in DOM
+const userContainer = document.querySelector("#user-container");
+
+const printUsers = async () => {
+  const users = await getUsers();
+
+  users.forEach((user) => {
+    const div = document.createElement("div");
+
+    const nameElement = document.createElement("h3");
+    nameElement.textContent = user.name;
+    div.appendChild(nameElement);
+
+    const emailElement = document.createElement("p");
+    emailElement.textContent = user.email;
+    div.appendChild(emailElement);
+
+    userContainer.appendChild(div);
+  });
+};
+
+printUsers();
