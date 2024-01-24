00f64d3ca78b07dc07754e31541ae6c01201a632
mat-afk <mateusc.ramos@outlook.com>
2024-01-24 17:32:51 -0300

    react-advancing: list rendering with keys

--- react/3-advancing/advancing/src/components/ListRender.jsx
+++ react/3-advancing/advancing/src/components/ListRender.jsx
@@ -2,12 +2,29 @@ import { useState } from "react";
 
 const ListRender = () => {
   const [list] = useState(["Markiplier", "Jacksepticeye", "Pewdiepie"]);
+  const [users] = useState([
+    { id: 1, name: "Markiplier", age: 29 },
+    { id: 2, name: "Jacksepticeye", age: 28 },
+    { id: 3, name: "Pewdiepie", age: 31 },
+  ]);
 
   return (
     <div>
+      {/* List rendering without keys */}
+      <h5>without keys</h5>
       <ul>
         {list.map((item) => (
           <li>{item}</li>
+        ))}
+      </ul>
+
+      {/* List rendering with keys */}
+      <h5>with keys</h5>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            Hello, my name is {user.name} and I am {user.age} years old
+          </li>
         ))}
       </ul>
     </div>
