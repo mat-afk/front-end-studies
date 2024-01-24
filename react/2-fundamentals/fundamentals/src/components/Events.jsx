a3aaeb498e287a4e3739359bf93ff25bb8bd7025
Mateus <mateusc.ramos@outlook.com>
2024-01-23 22:16:23 -0300

    react-fundamentals: events with functions

--- react/2-fundamentals/fundamentals/src/components/Events.jsx
+++ react/2-fundamentals/fundamentals/src/components/Events.jsx
@@ -1,6 +1,13 @@
 const Events = () => {
+  const button = document.querySelector("button");
+  let count = 0;
+  const handleClick = () => {
+    button.textContent = `Count ${++count}`;
+  };
+
   return (
     <div>
+      <button onClick={handleClick}>Count {count}</button>
       <button onClick={() => console.log("Thanks for clicking me =)")}>
         Click me
       </button>
