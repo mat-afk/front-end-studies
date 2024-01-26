ee9729dfce3ccccb829919db3c3f760dfeaf29c7
mat-afk <mateusc.ramos@outlook.com>
2024-01-26 19:24:21 -0300

    react-forms: inputs inside labels

--- react/5-react-forms/react-forms/src/components/Form.jsx
+++ react/5-react-forms/react-forms/src/components/Form.jsx
@@ -8,6 +8,13 @@ const Form = () => {
           <label htmlFor="name">Name:</label>
           <input type="text" name="name" placeholder="Enter your name" />
         </div>
+        {/* Inputs inside labels */}
+        <div>
+          <label>
+            <span>E-mail:</span>
+            <input type="email" name="email" placeholder="Enter your email" />
+          </label>
+        </div>
         <input type="submit" value="Submit" />
       </form>
     </div>
