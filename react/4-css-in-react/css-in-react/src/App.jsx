147994d186d403e4ea9c5dc38777b47874de94c1
mat-afk <mateusc.ramos@outlook.com>
2024-01-26 16:02:59 -0300

    css-in-react: global css

---
+++ react/4-css-in-react/css-in-react/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* Global CSS */}
+        <h1>CSS in React</h1>
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
