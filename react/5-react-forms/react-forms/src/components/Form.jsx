0d8bd62c94f5588a0ec6c6e7aeefa7297037195a
mat-afk <mateusc.ramos@outlook.com>
2024-01-26 19:52:36 -0300

    react-forms: data management with useState

--- react/5-react-forms/react-forms/src/components/Form.jsx
+++ react/5-react-forms/react-forms/src/components/Form.jsx
@@ -1,18 +1,35 @@
 import "./Form.css";
+import { useState } from "react";
 
 const Form = () => {
+  // Data management with useState
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  console.log(name, email);
+
   return (
     <div>
       <form>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" placeholder="Enter your name" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         {/* Inputs inside labels */}
         <div>
           <label>
             <span>E-mail:</span>
-            <input type="email" name="email" placeholder="Enter your email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </label>
         </div>
         <input type="submit" value="Submit" />
