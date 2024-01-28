cdeb1d454084ff1614e5e07eb887c01bdb83b151
Mateus <mateusc.ramos@outlook.com>
2024-01-28 18:08:01 -0300

    http-react: fetching API data

--- react/6-http-react/http-react/src/App.jsx
+++ react/6-http-react/http-react/src/App.jsx
@@ -1,11 +1,35 @@
-import './App.css'
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
+
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <div>
       <h1>HTTP in React</h1>
+      {/* Fetching API data */}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$ {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
