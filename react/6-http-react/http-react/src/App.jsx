903c62e39e455a0dd78a27464d8fb2f946120392
Mateus <mateusc.ramos@outlook.com>
2024-01-28 19:12:05 -0300

    http-react: posting data in API

--- react/6-http-react/http-react/src/App.jsx
+++ react/6-http-react/http-react/src/App.jsx
@@ -5,6 +5,7 @@ const url = "http://localhost:3000/products";
 import "./App.css";
 
 function App() {
+  // GET - Fetching API data
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -17,10 +18,26 @@ function App() {
     getProducts();
   }, []);
 
+  // POST - Creating a new product
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = { name, price };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(product),
+    });
+  };
+
   return (
     <div>
       <h1>HTTP in React</h1>
-      {/* Fetching API data */}
+      {/* GET - Fetching API data */}
       <ul>
         {products.map((product) => (
           <li key={product.id}>
@@ -28,6 +45,28 @@ function App() {
           </li>
         ))}
       </ul>
+      {/* POST - Creating a new product */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Name</span>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Price</span>
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <button type="submit">Add product</button>
+        </form>
+      </div>
     </div>
   );
 }
