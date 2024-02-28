bf950ad0d9ab6fe8492c914d384ac2451cbde53e
Mateus <mateusc.ramos@outlook.com>
2024-02-28 00:17:42 -0300

    hooks: useEffect

---
+++ react/9-hooks/hooks/src/components/HookUseEffect.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+const HookUseEffect = () => {
+  const [number, setNumber] = useState(0);
+
+  const increaseNumber = () => {
+    setNumber(number + 1);
+  };
+
+  const [anotherNumber, setAnotherNumber] = useState(10);
+
+  const decreaseAnotherNumber = () => {
+    setAnotherNumber(anotherNumber - 1);
+  };
+
+  // without dependencies
+  useEffect(() => {
+    console.log("use effect without dependencies");
+  });
+
+  // with empty array
+  useEffect(() => {
+    console.log("use effect with empty array");
+  }, []);
+
+  // with dependencies array
+  useEffect(() => {
+    console.log("use effect with dependencies array");
+  }, [anotherNumber]);
+
+  // clean up
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("use effect clean up");
+      setNumber(number * 1);
+    }, 2000);
+    
+    return () => clearTimeout(timer);
+  }, [number]);
+
+  return (
+    <div>
+      <h3>useEffect</h3>
+      <p>Number: {number}</p>
+      <button onClick={increaseNumber}>Increase</button>
+      <p>Another number: {anotherNumber}</p>
+      <button onClick={decreaseAnotherNumber}>Decrease</button>
+    </div>
+  );
+};
+
+export default HookUseEffect;
